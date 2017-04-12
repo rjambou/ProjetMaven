8de1b6238d036d27f4ffc024013a9984c6516e11
rjambou <remi.jambou@insa-cvl.fr>
2017-04-12 19:19:35 +0200

    deazeaca

--- rj/src/main/java/rj/App.java
+++ rj/src/main/java/rj/App.java
@@ -1,5 +1,18 @@
 package rj;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Vector;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.Predicate;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+
 /**
  * Hello world!
  *
@@ -13,12 +26,46 @@ public class App
     	monApp = new App();
         System.out.println( "Hello World, it's me, Remi !" );
         monApp.max(4, 5);
+        int monmax = 0;
+        try{
+        CSVReader reader = new CSVReader(new FileReader("data.csv"));
+    	CSVWriter writer = new CSVWriter(new FileWriter("data-filtered.csv"));
+        List<String[]> myEntries = reader.readAll();
+        for (String[] ligne : myEntries){
+        	List<String> list = Arrays.asList(ligne);
+        	Vector<String> out=new Vector <String>();
+        	CollectionUtils.select(list, new Predicate<String>() {
+				
+				public boolean evaluate(String arg0) {
+					// TODO Auto-generated method stub
+					int test = Integer.parseInt(arg0);
+					if (test>50){
+						return false;
+					}
+					return true;
+				}
+			},out);
+        	System.out.println("OUT : " + out);
+        	String[] tab_out = out.toArray(new String[0]);
+        	writer.writeNext(tab_out);
+        	for (String element : ligne){
+        		int nb=Integer.parseInt(element);
+        		monmax=monApp.max(monmax,nb);
+        	}
+        }
+        reader.close();
+    	writer.close();
+        }
+        catch(IOException e){
+        	System.out.println("Erreur de fichier.");
+        }
+        System.out.println(monmax);
     }
 
     
     public int max(int a, int b)
     {
-    	return a;
+    	return a > b ? a : b;
     }
 	
 }
